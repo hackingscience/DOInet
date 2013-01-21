d4e1347a6cf49c6b661119b21aa22950badddb97
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2013-01-21 17:42:41 -0500

    adds two convenience methods to publication model for getting publications cited and publications cited by

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -3,4 +3,16 @@ class Publication < ActiveRecord::Base
   has_many :citations, foreign_key: "citer_id", dependent: :destroy
 
   validates :doi, :format => { :with => %r{\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'<>])[[:graph:]])+)\b}, :message => "DOI must have valid format" } #"
+
+  def cited
+    Citation.find_all_by_citer_id(self.id).map do |c|
+      Publication.find(c.cited_id)
+    end
+  end
+
+  def cited_by
+    Citation.find_all_by_cited_id(self.id).map do |c|
+      Publication.find(c.citer_id)
+    end
+  end
 end
