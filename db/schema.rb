40091f92705dd13ebcb3a7e048a6f869602cfbe4
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2013-01-21 14:24:58 -0500

    updates db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130121192305) do
+
+  create_table "publications", :force => true do |t|
+    t.string   "doi"
+    t.string   "cites"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
